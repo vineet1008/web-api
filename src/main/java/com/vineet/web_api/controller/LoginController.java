625898e2993edc1cf91ab5e8e3e03c2ce8bfd4c0
vineet1008 <157875001+vineet1008@users.noreply.github.com>
2026-05-02 12:39:20 +0530

    Comment out RequestMapping in LoginController
    Commented out the RequestMapping annotation for the LoginController.

--- src/main/java/com/vineet/web_api/controller/LoginController.java
+++ src/main/java/com/vineet/web_api/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/web-api/")
+//@RequestMapping("/web-api/")
 public class LoginController {
 
     @GetMapping
