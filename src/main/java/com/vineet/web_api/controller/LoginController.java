b0d8e330ae7707a16ab44d6eb0cb5ff6bb41e60f
Vineet <guptavineet287@gmail.com>
2026-05-02 12:30:03 +0530

    Initial Commit

---
+++ src/main/java/com/vineet/web_api/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.vineet.web_api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/web-api/")
+public class LoginController {
+
+    @GetMapping
+    public String home(){
+        return "Hey This is the Spring boot App";
+    }
+
+
+}
