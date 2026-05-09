c266df638c4adaf6ed02ac67cb397224c42f7cf9
Vineet <guptavineet287@gmail.com>
2026-05-09 19:31:56 +0530

    keys are removed

--- src/main/java/com/vineet/web_api/config/SecurityConfig.java
+++ src/main/java/com/vineet/web_api/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.vineet.web_api.config;
 import com.vineet.web_api.filter.JwtFilter;
 import com.vineet.web_api.users.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
